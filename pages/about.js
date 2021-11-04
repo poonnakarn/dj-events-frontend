0abbddb6c7af1f5bfdcd6acf775175fb1918da31
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2021-11-04 22:42:46 +0700

    Add layout into all pages

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,10 @@
-import Link from "next/link";
-
+import Layout from "../components/layout";
 export default function AboutPage() {
   return (
-    <div>
+    <Layout title="About DJ Events">
       <h1>About</h1>
       <p>This is an app to find the latest DJ and other musical events</p>
       <p>Version: 1.0.</p>
-      <Link href="/">Home</Link>
-    </div>
+    </Layout>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Link from "next/link";
+import Layout from "../components/layout";
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>About</h1>
-      <p>Test Index</p>
-      <Link href="/about">About</Link>
-    </div>
+    <Layout>
+      <h1>Home</h1>
+    </Layout>
   );
 }
