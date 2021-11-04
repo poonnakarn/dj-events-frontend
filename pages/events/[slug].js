9fe01f08d43294dbb93a35d2a3c4e2d2c0859350
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2021-11-04 21:16:08 +0700

    Use useRouter hook

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,13 @@
+import { useRouter } from "next/router";
+
 export default function EventPage() {
+  const router = useRouter();
+  console.log(router);
   return (
     <div>
-      <h1>Event Page Slug</h1>
+      <h1>My Event</h1>
+      <h3>{router.query.slug}</h3>
+      <button onClick={() => router.push("/")}>Click</button>
     </div>
-  )
+  );
 }
